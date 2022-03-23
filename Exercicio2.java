5a7b14a260f8d6de2f469cddeefd9d638ae99c7b
PedroMarineli <phmo.1606@gmail.com>
2022-03-23 17:53:14 +0000

    ererer

--- Exercicio2.java
+++ Exercicio2.java
@@ -13,7 +13,7 @@ public class Exercicio2 {
             maiorValor = vetor[i];
         }
         
-        
+        System.out.println("Menor Valor: " + maiorValor);   
         System.out.println("Maior Valor: " + menorValor);    
     }   
 }
