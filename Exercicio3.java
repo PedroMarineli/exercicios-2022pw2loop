3ccc4665cbfe0cba5510315ce7ff71acbc5e4d59
PedroMarineli <phmo.1606@gmail.com>
2022-03-28 17:02:50 +0000

    fh

--- Exercicio3.java
+++ Exercicio3.java
@@ -1,5 +1,10 @@
 public class Exercicio3 {
     public static void main(String[] args) {
-          
+        int vetor[] = {15, 20, 55, 10, 85, 36, 77, 56, 66, 25};  
+        
+         for(int i=0;i<vetor.length;i++)
+        {
+            System.out.println(vetor[i]);
+        }
     }   
 }
