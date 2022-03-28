48a04d6c90415f4406dbf64ff19680fcccc44c6b
PedroMarineli <phmo.1606@gmail.com>
2022-03-28 17:29:58 +0000

    vg

---
+++ Exercicio4.java
@@ -0,0 +1,11 @@
+public class Exercicio3 {
+    public static void main(String[] args) {
+        int vetor[] = {16, 23, 55, 11, 85, 36, 77, 59, 66, 29};  
+        
+         for(int i = 0; i < 10; i++){
+            if(vetor[i] < menorValor){
+                menorValor = vetor[i];
+            }
+    }   
+}
+}
