0c2f03d1cf4c508b01c437bd5b253a7a018778da
PedroMarineli <phmo.1606@gmail.com>
2022-03-28 17:14:04 +0000

    efef

--- Exercicio3.java
+++ Exercicio3.java
@@ -2,8 +2,16 @@ public class Exercicio3 {
     public static void main(String[] args) {
         int vetor[] = {15, 20, 55, 10, 85, 36, 77, 56, 66, 25};  
         
-         for(int i=0;i<vetor.length;i++)
-        {
+         for(int i=0;i<vetor.length;i++){
+            if(vetor[i] > vetor[i+1]) {
+                int aux = vetor[i];
+                vetor[i] = vetor[i+1];
+                vetor[i+1] = aux;
+                i = -1;
+            }
+        }   
+
+         for(int i=0;i<vetor.length;i++){
             System.out.println(vetor[i]);
         }
     }   
