ff8c43bf65574814d05747170f918a7ac0d90798
PedroMarineli <phmo.1606@gmail.com>
2022-03-30 00:52:27 +0000

    dsg

--- Exercicio3.java
+++ Exercicio3.java
@@ -1,18 +1,15 @@
-public class Exercicio3 {
-    public static void main(String[] args) {
-        int vetor[] = {15, 20, 55, 10, 85, 36, 77, 56, 66, 25};  
-        
-         for(int i=0;i<vetor.length - 1;i++){
-            if(vetor[i] > vetor[i+1]) {
-                int aux = vetor[i];
-                vetor[i] = vetor[i+1];
-                vetor[i+1] = aux;
-                i = -1;
+public class exercicio3{
+    public static void main(String[] args){
+      int valores[] = {15, 20, 55, 10, 85, 36, 77, 56, 66, 25};
+            for(int i = 0;i < valores.length - 1;i++){
+                if(valores[i] > valores[i+1]){
+                    int aux = valores[i];
+                    valores[i] = valores[i+1];
+                    valores[i+1] = aux;
+                    i = -1;
+                }
             }
-        }   
-
-         for(int i=0;i<vetor.length;i++){
-            System.out.println(vetor[i]);
+      for(int i = 0;i < valores.length;i++) {
+          System.out.println(valores[i]);
         }
-    }   
-}
+    }
--- Exercicio4.java
+++ Exercicio4.java
@@ -1,11 +1,18 @@
-public class Exercicio3 {
-    public static void main(String[] args) {
-        int vetor[] = {16, 23, 55, 11, 85, 36, 77, 59, 66, 29};  
+public class exercicio4 {
+    public static void main(String[] args){
+        int numeros[] = {16, 23, 55, 11, 85, 36, 77, 59, 66, 29};
         
-         for(int i = 0; i < 10; i++){
-            if(vetor[i] < menorValor){
-                menorValor = vetor[i];
+        for(int i = 0;i < numeros.length;i++){
+            boolean ePrimo = true;
+            for(int valor = 2;valor < numeros[i];valor++){
+                if(numeros[i] % valor == 0){
+                    ePrimo = false;
+                    break;
+                }
             }
-    }   
-}
+            if(ePrimo){
+                System.out.println(numeros[i]);
+            }
+        }
+    }
 }
